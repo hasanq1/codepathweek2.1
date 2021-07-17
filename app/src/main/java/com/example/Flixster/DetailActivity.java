8d05afd11605c937cd9d7dbb39158c03db6da3a1
Hasan Qureshi <hasanq36@gmail.com>
2021-07-16 20:42:19 -0500

    UPDATED assignment 2

---
+++ app/src/main/java/com/example/Flixster/DetailActivity.java
@@ -0,0 +1,92 @@
+package com.example.Flixster;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.Flixster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class DetailActivity extends YouTubeBaseActivity {
+
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+    YouTubePlayerView youTubePlayerView;
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDdCtrC3jGLwUA54vLd5lJ2uIQ9fVxHbMc";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView =
+                (YouTubePlayerView) findViewById(R.id.player);
+
+//        String title = getIntent().getStringExtra("title");
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getRating());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    if (results.length() == 0){
+                        return;
+                    }
+                    String youtubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youtubeKey);
+                    inititalizeYoutube(youtubeKey);
+                } catch (JSONException e) {
+                    Log.e("AsyncHttpClient", "Failed to parse JSON");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d("AsyncHttpClient", "onFailure");
+            }
+        });
+    }
+
+    private void inititalizeYoutube(final String youtubeKey) {
+        youTubePlayerView.initialize("YOUR API KEY",
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+
+                        // do any work here to cue video, play video, etc.
+                        Log.d("DetailActivity", "onSuccess");
+                        youTubePlayer.cueVideo(youtubeKey);
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+                        Log.d("DetailActivity", "onFailure");
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/Flixster/adapters/MovieAdapter.java
@@ -0,0 +1,100 @@
+package com.example.Flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.Flixster.DetailActivity;
+import com.example.Flixster.R;
+import com.example.Flixster.models.Movie;
+
+import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import static android.media.CamcorderProfile.get;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        //get the movie at the passed in position
+        Movie movie = movies.get(position);
+        // bind the movie data into the VH
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        RelativeLayout container;
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+
+            String imageUrl;
+
+            //if phone is in landscape -> imageUrl = backdrop image
+            //else -> default poster image
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            } else{
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.placeholder).into(ivPoster);
+
+            // 1. register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    // 2. naviagte to ta new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+        }
+    }
+}
